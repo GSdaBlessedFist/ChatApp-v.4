a4e23ea2654f2d40ec081ab5999eebb5e8adcee6
GSdaBlessedFist <gsdablessedfist@gmail.com>
2021-05-21 01:46:54 -0500

    Version4:: Handlebars + Socket.io, 1)Structure

---
+++ server.js
@@ -0,0 +1,44 @@
+const express = require('express');
+const app = express();
+const hbs = require('hbs');
+const morgan = require('morgan');
+const path = require('path');
+const server = require('http').Server(app);
+const io = require('socket.io')(server);
+
+const port = process.env.PORT || 3100;
+const nameofApp = "chats a lot";
+
+app.use(express.static('public'));
+// app.use(morgan('tiny'));
+
+// Express config
+const publicDirectory = path.join(__dirname, "./public");
+const viewsPath = path.join(__dirname, "./src/templates/views");
+const partialsPath = path.join(__dirname, "./src/templates/partials");
+
+// Handlebars engine and views location
+app.set("view engine", "hbs");
+app.set("views", viewsPath);
+hbs.registerPartials(partialsPath);
+
+app.get("/",(req,res)=>{
+    res.render("index",{
+            title: nameofApp,
+            clientname: "clientName"
+    })
+})
+
+//Whenever someone connects this gets executed
+io.on('connection', function(socket) {
+   socket.emit('welcome',{message:"Welcome"})
+
+
+   socket.on('disconnect', () => {
+    console.log('user disconnected');
+   });
+});
+
+app.listen(port, () => {
+  console.log(`Red to go on port ${port}!`)
+});
